6a1bf3f694795902e63544fdfd79f5d68b8ccf46
Lalitha Nochili <lanochil@microsoft.com>
2020-04-13 01:33:16 +0530

    Update prereq-script.sh

--- .github/workflows/prereq-script.sh
+++ .github/workflows/prereq-script.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/sh
+                az role assignment create --assignee cf32a0cc-373c-47c9-9156-0db11f6a6dfc --role Contributor \
+                    --scope /subscriptions/$creds.subscriptionId/resourceGroups/$env.distributorResourceGroup
+                az group create -n $env.distributor_resource_group -l $env.image_location
+                az sig create -g $env.distributor_resource_group  --gallery-name $env.image_gallery_name
+                az sig image-definition create -g $env.distributor_resource_group --gallery-name $env.image_gallery_name \
+                  --gallery-image-definition $env.image_definiation --publisher $publisher --offer $offer  \
+                      --sku $sku  --os-type $os_type
