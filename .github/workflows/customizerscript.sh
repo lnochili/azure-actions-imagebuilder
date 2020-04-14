feb40ef25dd4954be5c3c3352e1987465e6b75f3
Lalitha Nochili <lanochil@microsoft.com>
2020-04-14 19:43:29 +0530

    Update customizerscript.sh

--- .github/workflows/customizerscript.sh
+++ .github/workflows/customizerscript.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/sh
+### Node.js Installation
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install nodejs
+node --version
 
-mkdir ./temp
-pwd > ./temp/temp.out
-echo "Hello .. custom script from image builder" >> /tmp/temp.out
+## Creating Hello World App
+sudo mkdir -p /var/www/myapp
+cd /var/www/myapp
+npm init --yes
+echo "var http = require('http'); 
+http.createServer(function(req,res){ 
+        res.writeHead(200, { 'Content-Type': 'text/plain' }); 
+        res.end('Hello World!');
+}).listen(8080);
+console.log('Server started on localhost:8080...!');"  > server.js
