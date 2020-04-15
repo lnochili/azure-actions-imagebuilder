4918174a11671b48e77aee8cd8a4f77ac13f8580
Lalitha Nochili <lanochil@microsoft.com>
2020-04-15 19:12:22 +0530

    Update customizerscript.sh

--- .github/workflows/customizerscript.sh
+++ .github/workflows/customizerscript.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
-### Node.js Installation
-sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt install nodejs
-sudo node --version
 
-## Creating Hello World App
 sudo mkdir -p /var/www/myapp
 sudo cd /var/www/myapp
-sudo npm init --yes
-sudo echo "var http = require('http'); 
-http.createServer(function(req,res){ 
-        res.writeHead(200, { 'Content-Type': 'text/plain' }); 
-        res.end('Hello World!');
-}).listen(8080);
-console.log('Server started on localhost:8080...!');"  > server.js
+sudo echo "Hello world" > myapp.out
+
